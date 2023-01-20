8568d980cff82d4b61fe14c094dd4518a756c322
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2023-01-20 22:02:50 +0900

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,36 @@ function Gallery(props) {
   );
 }
 
+function Form(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { breed } = event.target.elements;
+    props.onFormSubmit(breed.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+              </select>
+            </div>
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-dark">
+              Reload
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function Main() {
   const [urls, setUrls] = useState(null);
 
@@ -59,8 +89,20 @@ function Main() {
     });
   }, []);
 
+  function reloadImages(breed) {
+    fetchImages(breed).then((urls) => {
+      setUrls(urls);
+    });
+  }
+
   return (
     <main>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages}/>
+        </div>
+      </section>
+
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
